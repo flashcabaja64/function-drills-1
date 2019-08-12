d55b156bc09cbb8e500f37cb706e856dec7d73f1
Alexander Fukui <alexander.fukui@gmail.com>
2019-08-12 15:03:04 -0500

    stuck atm

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 'use strict';
 
 function createGreeting(name, age) {
+    if (!name && !age){
+        throw new Error("Arguments not valid");
+    }
     const yearOfBirth = 2019 - age;
     return `Hi, my name is Chris and I'm 29 years old. I was born in ${yearOfBirth}`;
 }
@@ -14,13 +17,12 @@ function yearOfBirth(age) {
         throw new Error("Age can not be negative");
     }
     return age;
-
-    try {
-        const greeting1 = createGreeting(age);
-    } catch(e) {
-        console.log(`${e.name}: ${e.message}`);
-    }
 }
 
-const greeting1 = createGreeting();
+try {
+    const greeting1 = createGreeting('Jeff', '29');
+} catch(e) {
+        console.log(`${e.name}: ${e.message}`);
+}
+
 console.log(greeting1);
