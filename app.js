61c2a3a671c106917bef9abe44c909761329a772
Hubert Yang <offspring7890@yahoo.com>
2019-08-12 12:24:59 -0700

    add try/catch/throw statement

--- app.js
+++ app.js
@@ -2,11 +2,24 @@
 
 function createGreeting(name, age) {
     const yearOfBirth = 2019 - age;
-    return `Hi, my name is Chris and I\'m 29 years old. I was born in ${yearOfBirth}`;
+    return `Hi, my name is Chris and I'm 29 years old. I was born in ${yearOfBirth}`;
 }
 
 function getYearOfBirth(age) {
     return 2019 - age;
+}
+
+function yearOfBirth(age) {
+    if (age < 0) {
+        throw new Error("Age can not be negative");
+    }
+    return age;
+
+    try {
+        const greeting1 = createGreeting(age);
+    } catch(e) {
+        console.log(`${e.name}: ${e.message}`);
+    }
 }
 
 const greeting1 = createGreeting();
