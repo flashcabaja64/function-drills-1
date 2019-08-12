53bf428e9f8a1421d53c122a127bc32c69610708
Hubert Yang <offspring7890@yahoo.com>
2019-08-12 11:58:54 -0700

    add yearOfBirth variable

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function createGreeting(name, age) {
-    return 'Hi, my name is Chris and I\'m 29 years old'
-};
+    const yearOfBirth = 2019 - age;
+    return `Hi, my name is Chris and I\'m 29 years old. I was born in ${yearOfBirth}`;
+}
 
 const greeting1 = createGreeting();
 console.log(greeting1);
